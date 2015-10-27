7abbb3f382a04a56db0e6581056fa3fc5859d5ef
Markus Dreyer <markus@madaxel.de>
2015-10-27 11:23:13 +0100

    autoloader not working

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,5 @@ if (!defined("TYPO3_MODE")) die ("Access denied.");
 
 if (TYPO3_MODE == 'FE') {
 	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('piwik') . 'Classes/UserFunc/Footer.php');
+	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('piwik').'Classes/PiwikApi/PiwikTracker.php');
 }
